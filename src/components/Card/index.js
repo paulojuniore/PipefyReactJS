9fe7fb911eb376ae3349e57da75dac6384c740ef
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-03-21 19:41:32 -0300

    Pegando referência do card que está sendo arrastado e do card em que o arrastado está passando por cima.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container, Label } from './styles';
 
-import { useDrag } from 'react-dnd';
+import { useDrag, useDrop } from 'react-dnd';
 
 export default function Card({ data }) {
+    const ref = useRef();
+
     const [{ isDragging }, dragRef] = useDrag({
-        item: { type: 'CARD' },
+        item: { type: 'CARD', id: data.id },
         collect: monitor => ({
             isDragging: monitor.isDragging(),
         })
     });
+    
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor){
+            console.log(item.id);
+            console.log(data.id);
+        }
+    });
+
+    dragRef(dropRef(ref));
 
     return (
-        <Container ref={dragRef} isDragging={isDragging}>
+        <Container ref={ref} isDragging={isDragging}>
             <header>
                 <Label color={data.labels} />
             </header>
