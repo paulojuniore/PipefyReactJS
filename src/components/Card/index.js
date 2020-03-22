98c88e71bc8ed815bdabfc7ca0e23e47b8bc78b1
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-03-22 16:57:39 -0300

    Drag'n Drop finalizado. OBS: não é possível ainda mover um card para uma lista vazia, porque isso só pode acontecer se o card passar por cima de outro card.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -25,7 +25,7 @@ export default function Card({ data, index, listIndex }) {
 
             const draggedIndex = item.index;
             const targetIndex = index;
-            if(draggedIndex === targetIndex){
+            if(draggedIndex === targetIndex && draggedListIndex === targetListIndex){
                 return;
             }
             
@@ -45,9 +45,10 @@ export default function Card({ data, index, listIndex }) {
                 return;
             }
 
-            move(draggedListIndex, draggedIndex, targetIndex);
+            move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
 
             item.index = targetIndex;
+            item.listIndex = targetListIndex;
         }
     });
 
